f32a511feee17e9373e0177a36bb8b1045444fb5
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-07-31 17:04:08 -0400

    tipo de acciones

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
+const INCREMENTAR = 'CONTADOR/INCREMENTAR'
+const DECREMENTAR = 'CONTADOR/DECREMENTAR'
+
+
 const initialState = 0
+
 export default function( state = initialState, action ) {
-    
-    console.log(action)
-    
     switch (action.type) {
-        case 'INCREMENTAR':
+        case INCREMENTAR:
             return state + 1
-        case 'DECREMENTAR':
+        case DECREMENTAR:
             return state - 1
         default:
             return state    
-        
-        
     }
 
 }
