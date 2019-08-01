a348ce66413974ab58f81ce7cd0020b4a6c28d5e
Mauricio Ojeda <m.ojeda.b@icloud.com>
2019-08-01 11:12:45 -0400

    conectando componentes a redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from './reducers'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './reducers';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,7 +10,7 @@ import * as serviceWorker from './serviceWorker';
 const store = createStore(reducer)
 
 ReactDOM.render(
-<Provider store={store}>
+<Provider store={ store } >
 <App />
 </Provider>
 , document.getElementById('root'));
